11dd156bdf19030b6760d08bf32c7f6c1ce8f226
Rigwarl <Avips@mail.ru>
2016-07-11 17:29:56 +0300

    wrap all with anonimous func

--- js/game.js
+++ js/game.js
@@ -1,349 +1,351 @@
-'use strict';
+(function(){
+  'use strict';
 
-var TILE_WIDTH = 100;
-var TILE_HEIGHT = 83;
+  var TILE_WIDTH = 100;
+  var TILE_HEIGHT = 83;
 
-var hudLevel = document.querySelector('#level');
-var hudKey = document.querySelector('#key');
+  var hudLevel = document.querySelector('#level');
+  var hudKey = document.querySelector('#key');
 
-var stage = new createjs.Stage('game-canvas');
-var queue = new createjs.LoadQueue();
+  var stage = new createjs.Stage('game-canvas');
+  var queue = new createjs.LoadQueue();
 
-var preloader;
-var hero;
-var key;
-var door;
-var bugs = [];
+  var preloader;
+  var hero;
+  var key;
+  var door;
+  var bugs = [];
 
-var level = 1;
-var keyCollected = false;
+  var level = 1;
+  var keyCollected = false;
 
-init();
+  init();
 
-function init() {
-  queue.addEventListener('complete', start);
-  queue.installPlugin(createjs.Sound);
-  queue.loadManifest([
-    { id: 'hero', src: 'img/char-boy.png' },
-    { id: 'grass', src: 'img/grass-block.png' },
-    { id: 'water', src: 'img/water-block.png' },
-    { id: 'stone', src: 'img/stone-block.png' },
-    { id: 'bug', src: 'img/enemy-bug.png' },
-    { id: 'key', src: 'img/key.png' },
-    { id: 'door', src: 'img/door.png' },
-    { id: 'screamSound', src: 'audio/man-scream.mp3' },
-    { id: 'keySound', src: 'audio/key-pickup.mp3' },
-    { id: 'splashSound', src: 'audio/water-splash.mp3' },
-    { id: 'doorSound', src: 'audio/door-open.mp3' },
-  ]);
+  function init() {
+    queue.addEventListener('complete', start);
+    queue.installPlugin(createjs.Sound);
+    queue.loadManifest([
+      { id: 'hero', src: 'img/char-boy.png' },
+      { id: 'grass', src: 'img/grass-block.png' },
+      { id: 'water', src: 'img/water-block.png' },
+      { id: 'stone', src: 'img/stone-block.png' },
+      { id: 'bug', src: 'img/enemy-bug.png' },
+      { id: 'key', src: 'img/key.png' },
+      { id: 'door', src: 'img/door.png' },
+      { id: 'screamSound', src: 'audio/man-scream.mp3' },
+      { id: 'keySound', src: 'audio/key-pickup.mp3' },
+      { id: 'splashSound', src: 'audio/water-splash.mp3' },
+      { id: 'doorSound', src: 'audio/door-open.mp3' },
+    ]);
 
-  createPreloader();
-  createTicker();
-}
+    createPreloader();
+    createTicker();
+  }
 
-function createPreloader() {
-  preloader = new createjs.Text('Loading... 0%', '35px Arial', '#000').set({
-    x: stage.canvas.width / 2,
-    y: stage.canvas.height / 2,
-    textAlign: 'center',
-    textBaseline: 'middle',
-  });
-  queue.addEventListener('progress', movePreloader);
+  function createPreloader() {
+    preloader = new createjs.Text('Loading... 0%', '35px Arial', '#000').set({
+      x: stage.canvas.width / 2,
+      y: stage.canvas.height / 2,
+      textAlign: 'center',
+      textBaseline: 'middle',
+    });
+    queue.addEventListener('progress', movePreloader);
 
-  stage.addChild(preloader);
-}
+    stage.addChild(preloader);
+  }
 
-function movePreloader(e) {
-  preloader.text = 'Loading... ' + Math.floor(e.progress * 100) + '%';
-}
+  function movePreloader(e) {
+    preloader.text = 'Loading... ' + Math.floor(e.progress * 100) + '%';
+  }
 
-function removePreloader() {
-  queue.removeEventListener('progress', movePreloader);
-  stage.removeChild(preloader);
-  preloader = null;
-}
+  function removePreloader() {
+    queue.removeEventListener('progress', movePreloader);
+    stage.removeChild(preloader);
+    preloader = null;
+  }
 
-function start() {
-  removePreloader();
-  createLevel();
-  createBugs();
-  createHero();
-  setLevel(1);
-  bindKeys();
-  bindTouch();
-}
+  function start() {
+    removePreloader();
+    createLevel();
+    createBugs();
+    createHero();
+    setLevel(1);
+    bindKeys();
+    bindTouch();
+  }
 
-function createLevel() {
-  for (var i = 0; i < 6; i++) {
-    var type = 'stone';
+  function createLevel() {
+    for (var i = 0; i < 6; i++) {
+      var type = 'stone';
 
-    if (i === 0) {
-      type = 'water';
-    } else if (i === 5) {
-      type = 'grass';
+      if (i === 0) {
+        type = 'water';
+      } else if (i === 5) {
+        type = 'grass';
+      }
+
+      for (var j = 0; j < 7; j++) {
+        createTile(type, i, j);
+      }
+
+      if (i === 0) {
+        createDoor();
+      }
     }
+    createKey();
+  }
 
-    for (var j = 0; j < 7; j++) {
-      createTile(type, i, j);
-    }
+  function createTile(type, i, j) {
+    var tile = new createjs.Bitmap(queue.getResult(type)).set({
+      x: j * TILE_WIDTH,
+      y: i * TILE_HEIGHT - 50,
+    });
+    stage.addChild(tile);
+  }
 
-    if (i === 0) {
-      createDoor();
+  function createDoor() {
+    door = new createjs.Bitmap(queue.getResult('door'));
+    door.y = -65;
+    stage.addChild(door);
+  }
+
+  function resetDoor() {
+    door.x = Math.floor(Math.random() * 6 + 1) * TILE_WIDTH;
+  }
+
+  function createKey() {
+    key = new createjs.Bitmap(queue.getResult('key'));
+    stage.addChild(key);
+  }
+
+  function resetKey() {
+    hudKey.innerHTML = '';
+    keyCollected = false;
+    key.visible = true;
+    key.set({
+      x: Math.floor(Math.random() * 6 + 1) * TILE_WIDTH,
+      y: Math.floor(Math.random() * 4) * TILE_HEIGHT,
+    });
+  }
+
+  function collectKey() {
+    createjs.Sound.play('keySound');
+    key.visible = false;
+    keyCollected = true;
+
+    hudKey.appendChild(queue.getResult('key'));
+  }
+
+  function createBugs() {
+    for (var i = 0; i < 5; i++) {
+      createBug();
     }
   }
-  createKey();
-}
 
-function createTile(type, i, j) {
-  var tile = new createjs.Bitmap(queue.getResult(type)).set({
-    x: j * TILE_WIDTH,
-    y: i * TILE_HEIGHT - 50,
-  });
-  stage.addChild(tile);
-}
+  function createBug() {
+    var bug = new createjs.Bitmap(queue.getResult('bug'));
+    setBug(bug);
 
-function createDoor() {
-  door = new createjs.Bitmap(queue.getResult('door'));
-  door.y = -65;
-  stage.addChild(door);
-}
-
-function resetDoor() {
-  door.x = Math.floor(Math.random() * 6 + 1) * TILE_WIDTH;
-}
-
-function createKey() {
-  key = new createjs.Bitmap(queue.getResult('key'));
-  stage.addChild(key);
-}
-
-function resetKey() {
-  hudKey.innerHTML = '';
-  keyCollected = false;
-  key.visible = true;
-  key.set({
-    x: Math.floor(Math.random() * 6 + 1) * TILE_WIDTH,
-    y: Math.floor(Math.random() * 4) * TILE_HEIGHT,
-  });
-}
-
-function collectKey() {
-  createjs.Sound.play('keySound');
-  key.visible = false;
-  keyCollected = true;
-
-  hudKey.appendChild(queue.getResult('key'));
-}
-
-function createBugs() {
-  for (var i = 0; i < 5; i++) {
-    createBug();
+    bugs.push(bug);
+    stage.addChild(bug);
   }
-}
 
-function createBug() {
-  var bug = new createjs.Bitmap(queue.getResult('bug'));
-  setBug(bug);
+  function setBug(bug) {
+    bug.set({
+      x: - (Math.random() + 1) * TILE_WIDTH,
+      y: Math.floor(Math.random() * 4) * TILE_HEIGHT,
+      speed: (Math.random() + 1) * (Math.random() + 1),
+    });
+  }
 
-  bugs.push(bug);
-  stage.addChild(bug);
-}
+  function resetBugs() {
+    bugs.splice(5).forEach(function(bug) {
+      stage.removeChild(bug);
+    });
+    bugs.forEach(setBug);
+  }
 
-function setBug(bug) {
-  bug.set({
-    x: - (Math.random() + 1) * TILE_WIDTH,
-    y: Math.floor(Math.random() * 4) * TILE_HEIGHT,
-    speed: (Math.random() + 1) * (Math.random() + 1),
-  });
-}
+  function createHero() {
+    hero = new createjs.Bitmap(queue.getResult('hero'));
+    stage.addChild(hero);
+  }
 
-function resetBugs() {
-  bugs.splice(5).forEach(function(bug) {
-    stage.removeChild(bug);
-  });
-  bugs.forEach(setBug);
-}
+  function resetHero() {
+    hero.x = TILE_WIDTH * 3;
+    hero.y = TILE_HEIGHT * 4;
+  }
 
-function createHero() {
-  hero = new createjs.Bitmap(queue.getResult('hero'));
-  stage.addChild(hero);
-}
+  function setLevel(lvl) {
+    level = lvl;
+    hudLevel.innerText = 'Level: ' + lvl;
 
-function resetHero() {
-  hero.x = TILE_WIDTH * 3;
-  hero.y = TILE_HEIGHT * 4;
-}
+    resetHero();
+    resetKey();
+    resetDoor();
+  }
 
-function setLevel(lvl) {
-  level = lvl;
-  hudLevel.innerText = 'Level: ' + lvl;
+  function bindKeys() {
+    var actions = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down',
+    };
 
-  resetHero();
-  resetKey();
-  resetDoor();
-}
+    window.addEventListener('keydown', function(e) {
+      moveHero(actions[e.keyCode]);
+    });
+  }
 
-function bindKeys() {
-  var actions = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down',
-  };
+  function bindTouch() {
+    var touch;
+    var x;
+    var y;
 
-  window.addEventListener('keydown', function(e) {
-    moveHero(actions[e.keyCode]);
-  });
-}
+    stage.canvas.addEventListener('touchstart', function(e) {
+      if (e.touches.length !== 1) {
+        return;
+      }
 
-function bindTouch() {
-  var touch;
-  var x;
-  var y;
+      touch = e.changedTouches[0];
+      x = touch.pageX;
+      y = touch.pageY;
+    });
 
-  stage.canvas.addEventListener('touchstart', function(e) {
-    if (e.touches.length !== 1) {
-      return;
-    }
+    stage.canvas.addEventListener('touchend', function(e) {
+      touch = e.changedTouches[0];
 
-    touch = e.changedTouches[0];
-    x = touch.pageX;
-    y = touch.pageY;
-  });
+      var dX = touch.pageX - x;
+      var dY = touch.pageY - y;
+      var action;
 
-  stage.canvas.addEventListener('touchend', function(e) {
-    touch = e.changedTouches[0];
-
-    var dX = touch.pageX - x;
-    var dY = touch.pageY - y;
-    var action;
-
-    if (Math.abs(dX) > Math.abs(dY)) {
-      if (dX > 0) {
-        action = 'right';
+      if (Math.abs(dX) > Math.abs(dY)) {
+        if (dX > 0) {
+          action = 'right';
+        } else {
+          action = 'left';
+        }
       } else {
-        action = 'left';
+        if (dY > 0) {
+          action = 'down';
+        } else {
+          action = 'up';
+        }
       }
-    } else {
-      if (dY > 0) {
-        action = 'down';
+
+      moveHero(action);
+    });
+
+    stage.canvas.addEventListener('touchmove', function(e) {
+      e.preventDefault();
+    });
+  }
+
+  function moveHero(action) {
+    var newX = hero.x;
+    var newY = hero.y;
+
+    switch (action) {
+      case 'left':
+        newX -= TILE_WIDTH;
+        break;
+      case 'right':
+        newX += TILE_WIDTH;
+        break;
+      case 'up':
+        newY -= TILE_HEIGHT;
+        break;
+      case 'down':
+        newY += TILE_HEIGHT;
+        break;
+    }
+
+    if (tryMove(newX, newY)) {
+      hero.x = newX;
+      hero.y = newY;
+    }
+  }
+
+  function tryMove(newX, newY) {
+    if (key.visible && checkCollision(key, { x: newX, y: newY })) {
+      collectKey();
+    }
+
+    if (newY < 0) {
+      if (hero.x === door.x) {
+        if (keyCollected) {
+          nextLevel();
+        }
       } else {
-        action = 'up';
+        dive();
+      }
+
+      return false;
+    }
+
+    return newX >= 0 && 
+           newX < stage.canvas.width && 
+           newY < stage.canvas.height - 100;
+  }
+
+  function checkCollision(obj1, obj2) {
+    obj2 = obj2 || hero;
+
+    return obj1.y === obj2.y && 
+           obj1.x + TILE_WIDTH * 0.75 > obj2.x && 
+           obj2.x + TILE_WIDTH * 0.75 > obj1.x;
+  }
+
+  function dive() {
+    createjs.Sound.play('splashSound');
+    resetHero();
+  }
+
+  function nextLevel() {
+    createjs.Sound.play('doorSound');
+    setLevel(++level);
+    console.log('level ' + level);
+
+    if (level % 3 === 0) {
+      createBug();
+    }
+  }
+
+  function gameOver() {
+    console.log('lost on lvl ' + level);
+    createjs.Sound.play('screamSound');
+    setLevel(1);
+    resetBugs();
+  }
+
+  function createTicker() {
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener('tick', function(e) {
+      if (e.delta > 1000) {
+        return;
+      }
+
+      moveBugs(e.delta);
+      checkHit();
+      stage.update();
+    });
+  }
+
+  function moveBugs(delta) {
+    for (var i = 0; i < bugs.length; i++) {
+      var bug = bugs[i];
+      bug.x += bug.speed * delta / 10;
+      if (bug.x > stage.canvas.width) {
+        setBug(bug);
       }
     }
-
-    moveHero(action);
-  });
-
-  stage.canvas.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-  });
-}
-
-function moveHero(action) {
-  var newX = hero.x;
-  var newY = hero.y;
-
-  switch (action) {
-    case 'left':
-      newX -= TILE_WIDTH;
-      break;
-    case 'right':
-      newX += TILE_WIDTH;
-      break;
-    case 'up':
-      newY -= TILE_HEIGHT;
-      break;
-    case 'down':
-      newY += TILE_HEIGHT;
-      break;
   }
 
-  if (tryMove(newX, newY)) {
-    hero.x = newX;
-    hero.y = newY;
-  }
-}
-
-function tryMove(newX, newY) {
-  if (key.visible && checkCollision(key, { x: newX, y: newY })) {
-    collectKey();
-  }
-
-  if (newY < 0) {
-    if (hero.x === door.x) {
-      if (keyCollected) {
-        nextLevel();
+  function checkHit() {
+    for (var i = 0; i < bugs.length; i++) {
+      if (checkCollision(bugs[i])) {
+        gameOver();
       }
-    } else {
-      dive();
-    }
-
-    return false;
-  }
-
-  return newX >= 0 && 
-         newX < stage.canvas.width && 
-         newY < stage.canvas.height - 100;
-}
-
-function checkCollision(obj1, obj2) {
-  obj2 = obj2 || hero;
-
-  return obj1.y === obj2.y && 
-         obj1.x + TILE_WIDTH * 0.75 > obj2.x && 
-         obj2.x + TILE_WIDTH * 0.75 > obj1.x;
-}
-
-function dive() {
-  createjs.Sound.play('splashSound');
-  resetHero();
-}
-
-function nextLevel() {
-  createjs.Sound.play('doorSound');
-  setLevel(++level);
-  console.log('level ' + level);
-
-  if (level % 3 === 0) {
-    createBug();
-  }
-}
-
-function gameOver() {
-  console.log('lost on lvl ' + level);
-  createjs.Sound.play('screamSound');
-  setLevel(1);
-  resetBugs();
-}
-
-function createTicker() {
-  createjs.Ticker.timingMode = createjs.Ticker.RAF;
-  createjs.Ticker.addEventListener('tick', function(e) {
-    if (e.delta > 1000) {
-      return;
-    }
-
-    moveBugs(e.delta);
-    checkHit();
-    stage.update();
-  });
-}
-
-function moveBugs(delta) {
-  for (var i = 0; i < bugs.length; i++) {
-    var bug = bugs[i];
-    bug.x += bug.speed * delta / 10;
-    if (bug.x > stage.canvas.width) {
-      setBug(bug);
     }
   }
-}
-
-function checkHit() {
-  for (var i = 0; i < bugs.length; i++) {
-    if (checkCollision(bugs[i])) {
-      gameOver();
-    }
-  }
-}
+})();
